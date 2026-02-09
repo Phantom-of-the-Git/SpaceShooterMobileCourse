7ce288651ac615a19cbc9878f48d87f021322dd4
Phantom-of-the-Git <mahmutkya1905@gmail.com>
2026-02-09 14:16:01 +0300

    Added MeteorSpawner

--- Assets/Scripts/Core/Enemy/Meteor.cs
+++ Assets/Scripts/Core/Enemy/Meteor.cs
@@ -4,6 +4,7 @@ public class Meteor : Enemy
 {
     [SerializeField] private float minSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float rotateSpeed;
     private float speed;
     void Start()
     {
@@ -12,6 +13,7 @@ public class Meteor : Enemy
     }
     void Update()
     {
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
     }
     public override void HurtSequence()
     {
@@ -27,5 +29,9 @@ public class Meteor : Enemy
         {
             Destroy(collision.gameObject);
         }
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Core/Enemy/MeteorSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] meteorPrefabs;
+    [SerializeField] private float spawnTime;
+
+    private float timer;
+    private int i;
+
+    private Camera mainCam;
+    private float maxLeft;
+    private float maxRight;
+    private float yPos;
+    void Start()
+    {
+        mainCam = Camera.main;
+        StartCoroutine(SetBoundaries());
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > spawnTime)
+        {
+            i = Random.Range(0, meteorPrefabs.Length);
+            GameObject tempMeteor = Instantiate(meteorPrefabs[i], new Vector3(Random.Range(maxLeft, maxRight), yPos, -5), Quaternion.Euler(0, 0, Random.Range(0, 360)));
+            float size = Random.Range(0.9f, 1.1f);
+            tempMeteor.transform.localScale = new Vector3(size, size, 0);
+            timer = 0;
+        }
+    }
+    private IEnumerator SetBoundaries()
+    {
+        yield return new WaitForSeconds(0.4f);
+        //Set boundaries of the movement for player through camera view point (0,0) to (1,1)
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f, 0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
+        yPos = mainCam.ViewportToWorldPoint(new Vector2(0, 1.1f)).y;
+    }
+}
