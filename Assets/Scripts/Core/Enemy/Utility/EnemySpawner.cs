681f87a97e9486d6741af05d3d3a64418e8dedac
Phantom-of-the-Git <mahmutkya1905@gmail.com>
2026-02-12 14:55:22 +0300

    Added enemy spawner and fixed explosion animation
    fixed the explosion animations leaving marks on the screen

--- Assets/Scripts/Core/Enemy/Utility/EnemySpawner.cs
+++ Assets/Scripts/Core/Enemy/Utility/EnemySpawner.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using UnityEngine;
 
-public class MeteorSpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] meteorPrefabs;
-    [SerializeField] private float spawnTime;
-
-    private float timer;
-    private int i;
-
     private Camera mainCam;
     private float maxLeft;
     private float maxRight;
     private float yPos;
+
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject[] enemies;
+    private float enemyTimer;
+    [Space(15)]
+    [SerializeField] private float enemySpawnTime;
     void Start()
     {
         mainCam = Camera.main;
@@ -20,14 +20,16 @@ public class MeteorSpawner : MonoBehaviour
     }
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > spawnTime)
+        EnemySpawn();
+    }
+    private void EnemySpawn()
+    {
+        enemyTimer += Time.deltaTime;
+        if (enemyTimer > enemySpawnTime)
         {
-            i = Random.Range(0, meteorPrefabs.Length);
-            GameObject tempMeteor = Instantiate(meteorPrefabs[i], new Vector3(Random.Range(maxLeft, maxRight), yPos, -5), Quaternion.Euler(0, 0, Random.Range(0, 360)));
-            float size = Random.Range(0.9f, 1.1f);
-            tempMeteor.transform.localScale = new Vector3(size, size, 0);
-            timer = 0;
+            int randPick = Random.Range(0, enemies.Length);
+            Instantiate(enemies[randPick], new Vector3(Random.Range(maxLeft, maxRight), yPos, 0), Quaternion.identity);
+            enemyTimer = 0;
         }
     }
     private IEnumerator SetBoundaries()
@@ -38,4 +40,5 @@ public class MeteorSpawner : MonoBehaviour
         maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
         yPos = mainCam.ViewportToWorldPoint(new Vector2(0, 1.1f)).y;
     }
+
 }
